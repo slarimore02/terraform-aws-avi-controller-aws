83fd50c3afa41b57851c95dcc4e9f57bd4a82f32
Sean Larimore <slarimore02@gmail.com>
2021-01-24 21:07:23 -0700

    First commit

--- data.tf
+++ data.tf
@@ -5,20 +5,3 @@ data "aws_subnet" "custom" {
   for_each = toset(var.custom_subnet_ids)
   id       = each.value
 }
-#data "aws_subnet" "selected" {
-#  count = var.create_networking ? 0 : 1
-#  filter {
-#    name   = "tag:Name"
-#    values = [var.custom_subnet_ids] # insert values here
-#  }
-#
-#locals {
-#  az_names = data.aws_availability_zones.azs.names
-#}
-#locals {
-#  "controller-ami-20.1.2" = {
-#    us-west-2 = ""
-#    us-west-1 = ""
-#
-#  }
-#}
